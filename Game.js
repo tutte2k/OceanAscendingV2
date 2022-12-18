16b3bf1230f9d7c73eccc01029c934303d0da1e0
tutte2k <45877330+tutte2k@users.noreply.github.com>
2022-12-18 10:16:33 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -329,8 +329,7 @@ window.addEventListener("load", function () {
     }
     findFocus(key) {
       let enemy = this.enemies.find((enemy) => {
-        if (enemy.x < this.width + this.enemy.width * 0.5)
-          return enemy.text.startsWith(key);
+        if (enemy.x < this.width) return enemy.text.startsWith(key);
       });
 
       if (enemy && !game.lose) {
