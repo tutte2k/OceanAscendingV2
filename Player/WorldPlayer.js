a8a285df058a3525058267cb52f79931ee35a280
tutte2k <45877330+tutte2k@users.noreply.github.com>
2023-01-13 17:25:58 +0100

    Update WorldPlayer.js

--- Player/WorldPlayer.js
+++ Player/WorldPlayer.js
@@ -36,7 +36,7 @@ export default class WorldPlayer {
     };
     this.moving = false;
     this.swimming = false;
-    this.speed = !window.location.href.includes("5500")
+    this.speed = window.location.href.includes("5500")
       ? 15
       : dataSource.getStore().shop.mapSpeed;
   }
@@ -47,7 +47,6 @@ export default class WorldPlayer {
     if (this.moving || this.swimming) {
       this.update(deltaTime);
     }
-
     if (this.swimming === true && this.state !== this.sprites.swim) {
       this.state = this.sprites.swim;
     } else if (this.swimming === false && this.state !== this.sprites.walk) {
